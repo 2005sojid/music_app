3f29a2b25573044de64819804f1e04af890ca21f
Sojid <2005sojid@gmail.com>
2024-03-13 23:14:40 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:project2/pages/home_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+        useMaterial3: true,
+        primaryColor: Colors.grey.shade300
+      ),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text("Music"),
+            centerTitle: true,
+            pinned: true,
+            snap: true,
+            floating: true,
+            backgroundColor: Theme.of(context).primaryColor,
+            bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(70),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide:
+                              const BorderSide(color: Colors.transparent)),
+                      fillColor: Colors.grey.shade500,
+                      filled: true,
+                      //prefixIcon: const Icon(Icons.search_rounded),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.clear_rounded),
+                        onPressed: _textController.clear,
+                      ),
+                      hintText: "Search music...",
+                      hintStyle:
+                          TextStyle(fontSize: 18, color: Colors.grey.shade700),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide:
+                              const BorderSide(color: Colors.transparent)),
+                    ),
+                  ),
+                )),
+          )
+        ],
+      ),
+    );
+  }
+}
