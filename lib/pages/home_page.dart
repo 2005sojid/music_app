ab11d819e2837736454d5e4e06af746a5c30e260
Sojid <2005sojid@gmail.com>
2024-03-13 23:27:46 +0500

    added text field style

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,24 +21,31 @@ class HomePage extends StatelessWidget {
                 preferredSize: const Size.fromHeight(70),
                 child: Padding(
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+                      const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15),
                   child: TextField(
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400
+                    ),
                     controller: _textController,
                     decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.all(10),
+
                       focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
                           borderSide:
                               const BorderSide(color: Colors.transparent)),
                       fillColor: Colors.grey.shade500,
                       filled: true,
-                      //prefixIcon: const Icon(Icons.search_rounded),
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.clear_rounded),
-                        onPressed: _textController.clear,
-                      ),
+                      prefixIcon: const Icon(Icons.search_rounded),
+                      // suffixIcon: IconButton(
+                      //   icon: const Icon(Icons.clear_rounded),
+                      //   onPressed: _textController.clear,
+                      // ),
                       hintText: "Search music...",
                       hintStyle:
                           TextStyle(fontSize: 18, color: Colors.grey.shade700),
+
                       enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
                           borderSide:
